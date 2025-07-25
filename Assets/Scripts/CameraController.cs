60602a72a73e0a2a3e645fde5e108c53a9fcbb00
SerBakon <serbakonyt@gmail.com>
2025-07-24 22:12:25 -0700

    feat: added sprinting

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -2,6 +2,8 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
+    [Header("Camera")]
+    [SerializeField] private Camera playerCam;
     void Start()
     {
         
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,15 +4,24 @@ public class PlayerMovement : MonoBehaviour
 {
     [Header("Movement")]
     [SerializeField] private float movementSpeed;
+
+    [Header("Player Inputs")]
+    [SerializeField] private KeyCode sprint;
     
     // Private variables
     private Rigidbody rb;
+
+    // Floats
+    private float walkingSpeed;
+    private float sprintingSpeed;
 
     // Vector3's
     private Vector3 movement;
 
     private void Start()
     {
+        walkingSpeed = movementSpeed;
+        sprintingSpeed = movementSpeed * 1.5f;
         rb = GetComponent<Rigidbody>();
     }
 
@@ -27,7 +36,12 @@ public class PlayerMovement : MonoBehaviour
     }
 
     private void moveCharacter(Vector3 direction) {
-        rb.linearVelocity = direction * movementSpeed * Time.fixedDeltaTime;
+        if(Input.GetKey(sprint)) {
+            movementSpeed = sprintingSpeed;
+        } else {
+            movementSpeed = walkingSpeed;
+        }
+            rb.linearVelocity = direction * movementSpeed * Time.fixedDeltaTime;
     }
 
     public float Velocity {
